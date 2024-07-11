8e499131657ea88ed06d15711912534d6b26aaa2
Keven Ike <kevenikepsilva@gmail.com>
2024-07-11 12:15:20 -0300

    subindo aplicação base

---
+++ app/src/main/java/br/com/stockflow/stockmanager_api/StockmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockflow.stockmanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanagerApiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/br/com/stockflow/stockmanager_api/StockmanagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockflow.stockmanager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmanagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
